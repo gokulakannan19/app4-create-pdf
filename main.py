c90e3883912a85196001ac0c1a0fc448ff57db04
gokulakannan19 <sgokulakannan19.jc@gmail.com>
2024-12-28 13:57:08 +0530

    pdf created by rendering data from csv

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
+import pandas as pd
 from fpdf import FPDF
 
-pdf = FPDF(orientation="L", unit="mm", format="A4")
+df = pd.read_csv("topics.csv")
 
-pdf.add_page()
-pdf.set_font(family="Times", style="B", size=12)
-pdf.cell(w=0, h=12, txt="Hi there", ln=1, border=1, align="L")
-pdf.cell(w=0, h=12, txt="Hello there", ln=1, border=1, align="L")
+pdf = FPDF(orientation="P", unit="mm", format="A4")
 
-
-pdf.add_page()
-pdf.set_font(family="Times", style="B", size=12)
-pdf.cell(w=0, h=12, txt="Hi there", ln=1, border=1, align="L")
-pdf.cell(w=0, h=12, txt="Hello there", ln=1, border=1, align="L")
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(255, 0 , 0)
+    pdf.cell(w=0, h=12, txt=row["Topic"], ln=1, border=0, align="L")
+    pdf.line(10, 21, 200, 21)
 
 pdf.output("output.pdf")
